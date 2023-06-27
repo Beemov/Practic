aef9152dd7d69a0d287da16543ef35bb6c226830
Zheleznoy <vzheleztt@gmail.com>
2023-06-27 22:29:21 +0300

    pyCharm configuration2

--- main.py
+++ main.py
@@ -16,28 +16,23 @@ class Main(QtWidgets.QWidget):
         # self.button.clicked.connect(self.set_rbutton)
 
         # TODO: enable line edit on radio button change
-        self.modbusProtocolAscii = QRadioButton("ASCII")
-        self.modbusProtocolAsciiSerial = QLineEdit()
-        self.modbusProtocolAsciiSerial.setPlaceholderText("COM1:")
-        self.modbusProtocolRtu = QRadioButton("RTU")
-        self.modbusProtocolRtuSerial = QLineEdit()
-        self.modbusProtocolRtuSerial.setPlaceholderText("COM1:")
+        # self.modbusProtocolAscii = QRadioButton("ASCII")
+        # self.modbusProtocolAsciiSerial = QLineEdit()
+        # self.modbusProtocolAsciiSerial.setPlaceholderText("COM1:")
+        # self.modbusProtocolRtu = QRadioButton("RTU")
+        # self.modbusProtocolRtuSerial = QLineEdit()
+        # self.modbusProtocolRtuSerial.setPlaceholderText("COM1:")
         self.modbusProtocolTcp = QRadioButton("TCP")
         self.modbusProtocolTcpIp = QLineEdit()
-        self.modbusProtocolTcpIp.setPlaceholderText("192.168.1.10")
-        self.modbusProtocolUdp = QRadioButton("UDP")
-        self.modbusProtocolUpdIp = QLineEdit()
-        self.modbusProtocolUpdIp.setPlaceholderText("192.168.1.11")
-        self.modbusProtocolEnc = QRadioButton("RTU over TCP")
-        self.modbusProtocolEncIp = QLineEdit()
-        self.modbusProtocolEncIp.setPlaceholderText("192.168.1.10")
+        self.modbusProtocolTcpIp.setPlaceholderText("127.0.0.1")
+        # self.modbusProtocolUdp = QRadioButton("UDP")
+        # self.modbusProtocolUpdIp = QLineEdit()
+        # self.modbusProtocolUpdIp.setPlaceholderText("192.168.1.11")
+        # self.modbusProtocolEnc = QRadioButton("RTU over TCP")
+        # self.modbusProtocolEncIp = QLineEdit()
+        # self.modbusProtocolEncIp.setPlaceholderText("192.168.1.10")
         #
-        # self.buttons_values = {
-        #     "1": self.r1,
-        #     "2": self.r2,
-        #     "3": self.r3,
-        #     "4": self.r4,
-        # }
+
 
         self.generateButton = QPushButton("generate")
         self.generateButton.clicked.connect(self.generateCommandLine)
@@ -48,46 +43,53 @@ class Main(QtWidgets.QWidget):
 
         g_layout = QGridLayout(self)
         #g_layout.addWidget(self.button, 1, 2)
-        g_layout.addWidget(self.modbusProtocolAscii, 2, 1)
-        g_layout.addWidget(self.modbusProtocolAsciiSerial, 2, 2)
-        g_layout.addWidget(self.modbusProtocolRtu, 3, 1)
-        g_layout.addWidget(self.modbusProtocolRtuSerial, 3, 2)
+        # g_layout.addWidget(self.modbusProtocolAscii, 2, 1)
+        # g_layout.addWidget(self.modbusProtocolAsciiSerial, 2, 2)
+        # g_layout.addWidget(self.modbusProtocolRtu, 3, 1)
+        # g_layout.addWidget(self.modbusProtocolRtuSerial, 3, 2)
         g_layout.addWidget(self.modbusProtocolTcp, 4, 1)
         g_layout.addWidget(self.modbusProtocolTcpIp, 4, 2)
-        g_layout.addWidget(self.modbusProtocolUdp, 5, 1)
-        g_layout.addWidget(self.modbusProtocolUpdIp, 5, 2)
-        g_layout.addWidget(self.modbusProtocolEnc, 6, 1)
-        g_layout.addWidget(self.modbusProtocolEncIp, 6, 2)
+        # g_layout.addWidget(self.modbusProtocolUdp, 5, 1)
+        # g_layout.addWidget(self.modbusProtocolUpdIp, 5, 2)
+        # g_layout.addWidget(self.modbusProtocolEnc, 6, 1)
+        # g_layout.addWidget(self.modbusProtocolEncIp, 6, 2)
         g_layout.addWidget(self.generateButton, 7, 1)
-        g_layout.addWidget(self.commandLine, 8, 1, 1, 2)
+        g_layout.addWidget(self.commandLine, 8, 1, 2, 2)
 
         self.show()
 
     def generateCommandLine(self):
-        line = "modpoll: " + self.generateModbusProtocol()
+        line = "modpoll " + self.generateModbusProtocol()
 
         self.commandLine.insertPlainText(line)
         pass
 
     def generateModbusProtocol(self):
         line = ""
-        if self.modbusProtocolAscii.isChecked():
-            line = line + "-m ascii " + self.modbusProtocolRtuSerial.text() + "\n"
-        if self.modbusProtocolRtu.isChecked():
-            line = line + "-m rtu " + self.modbusProtocolTcpIp.text() + "\n"
+        i = 0
+        # if self.modbusProtocolAscii.isChecked():
+        #     line = line + "-m ascii " + self.modbusProtocolRtuSerial.text() + "\n"
+        # if self.modbusProtocolRtu.isChecked():
+        #     line = line + "-m rtu " + self.modbusProtocolTcpIp.text() + "\n"
         if self.modbusProtocolTcp.isChecked():
+            i+=1
+            if not self.modbusProtocolTcpIp.text():
+                self.modbusProtocolTcpIp.setText("127.0.0.1")
             line = line + "-m tcp " + self.modbusProtocolTcpIp.text() + "\n"
-        if self.modbusProtocolUdp.isChecked():
-            line = line + "-m udp " + self.modbusProtocolTcpIp.text() + "\n"
-        if self.modbusProtocolEnc.isChecked():
-            line = line + "-m enc " + self.modbusProtocolTcpIp.text() + "\n"
+        # if self.modbusProtocolUdp.isChecked():
+        #     line = line + "-m udp " + self.modbusProtocolTcpIp.text() + "\n"
+        # if self.modbusProtocolEnc.isChecked():
+        #     line = line + "-m enc " + self.modbusProtocolTcpIp.text() + "\n"
+        if i != 1 :
+            self.modbusProtocolTcpIp.setText("127.0.0.1")
+            line = line + "-m tcp " + self.modbusProtocolTcpIp.text() + "\n"
 
         return line
 
     # TODO: block
-    def blockProtocol(self):
-        if self.modbusProtocolAscii.isChecked():
-            pass
+    # def blockProtocol(self):
+    #     if self.modbusProtocolAscii.isChecked():
+    #         pass
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
